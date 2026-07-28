9fbc7e3ba304d4b6cf79c1d02d2117e7d62c4d51
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2026-07-28 16:42:56 +0500

    feat: add wasm to resolver source extensions in metro configuration

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for wasm files to resolver asset extensions
+// Add support for wasm files to resolver asset and source extensions
 config.resolver.assetExts.push('wasm');
+config.resolver.sourceExts.push('wasm');
 
 // Enhance middleware to add Cross-Origin headers required for SharedArrayBuffer / WASM Web Workers
 config.server.enhanceMiddleware = (middleware) => {
